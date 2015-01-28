eac07066412eeb8d56c2b53721345c92719986cd
Jacob Carlsson <jacob.carlsson@gmail.com>
2015-01-28 09:22:12 +0100

    Changed tabs to spaces

--- test/streamSpec.js
+++ test/streamSpec.js
@@ -5,63 +5,63 @@ var through2 = require('through2');
 
 // Saves everything in .result
 function StringStream() {
-	this.result = '';
+  this.result = '';
 }
 
 StringStream.prototype.createStream = function() {
-	var _this = this;
+  var _this = this;
 
-	return through2({}, function(chunk, enc, callback) {
-		_this.result += chunk.toString();
-		callback(null, chunk);
-	}, function(callback) {
-		callback();
-	});
+  return through2({}, function(chunk, enc, callback) {
+    _this.result += chunk.toString();
+    callback(null, chunk);
+  }, function(callback) {
+    callback();
+  });
 }
 
 describe('stream', function() {
 
-	it('should encode a stream', function(done) {
+  it('should encode a stream', function(done) {
 
-		fs.readFile(__dirname + '/lorem-ipsum.txt', 'utf8', function(err, text) {
-			if(err) return console.error(err);
+    fs.readFile(__dirname + '/lorem-ipsum.txt', 'utf8', function(err, text) {
+      if(err) return console.error(err);
 
-			var encoded = morse.encode(text);
+      var encoded = morse.encode(text);
 
-			var stringStream = new StringStream();
+      var stringStream = new StringStream();
 
-			// Test it with streams
-			var readStream = fs.createReadStream(__dirname + '/lorem-ipsum.txt', {encoding: 'utf8'});
-			readStream
-				.pipe(morse.createEncodeStream())
-				.pipe(stringStream.createStream())
-				.on('finish', function() {
+      // Test it with streams
+      var readStream = fs.createReadStream(__dirname + '/lorem-ipsum.txt', {encoding: 'utf8'});
+      readStream
+        .pipe(morse.createEncodeStream())
+        .pipe(stringStream.createStream())
+        .on('finish', function() {
 
-					assert.equal(stringStream.result, encoded);
-					done();
-				});
-		});
-	});
+          assert.equal(stringStream.result, encoded);
+          done();
+        });
+    });
+  });
 
 
-	it('should decode a stream', function(done) {
-		fs.readFile(__dirname + '/lorem-ipsum-encoded.txt', 'utf8', function(err, text) {
-			if(err) return console.error(err);
+  it('should decode a stream', function(done) {
+    fs.readFile(__dirname + '/lorem-ipsum-encoded.txt', 'utf8', function(err, text) {
+      if(err) return console.error(err);
 
-			var decoded = morse.decode(text);
+      var decoded = morse.decode(text);
 
-			var stringStream = new StringStream();
+      var stringStream = new StringStream();
 
-			// Test it with streams
-			var readStream = fs.createReadStream(__dirname + '/lorem-ipsum-encoded.txt', {encoding: 'utf8'});
-			readStream
-				.pipe(morse.createDecodeStream())
-				.pipe(stringStream.createStream())
-				.on('finish', function() {
+      // Test it with streams
+      var readStream = fs.createReadStream(__dirname + '/lorem-ipsum-encoded.txt', {encoding: 'utf8'});
+      readStream
+        .pipe(morse.createDecodeStream())
+        .pipe(stringStream.createStream())
+        .on('finish', function() {
 
-					assert.equal(stringStream.result, decoded);
-					done();
-				});
-		});
-	});
+          assert.equal(stringStream.result, decoded);
+          done();
+        });
+    });
+  });
 });
